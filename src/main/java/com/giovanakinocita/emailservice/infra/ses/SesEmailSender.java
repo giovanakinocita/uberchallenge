ed5f895b34eec7e4ba9577b098dedd37e5c69916
Giovana Kinocita <giovanakinocita@hotmail.com>
2023-11-30 17:22:12 -0300

    Amazon Email Service

--- src/main/java/com/giovanakinocita/emailservice/infra/ses/SesEmailSender.java
+++ src/main/java/com/giovanakinocita/emailservice/infra/ses/SesEmailSender.java
@@ -1,4 +1,24 @@
 package com.giovanakinocita.emailservice.infra.ses;
 
-public class SesEmailSender {
+import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
+import com.amazonaws.services.simpleemail.model.*;
+import com.giovanakinocita.emailservice.adapters.EmailSenderGateway;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class SesEmailSender implements EmailSenderGateway {
+    private final AmazonSimpleEmailService amazonSimpleEmailService;
+
+    @Autowired
+    public SesEmailSender(AmazonSimpleEmailService amazonSimpleEmailService){
+        this.amazonSimpleEmailService = amazonSimpleEmailService;
+    }
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        SendEmailRequest request = new SendEmailRequest()
+                .withSource("giovanakinocita@hotmail.com")
+                .withDestination(new Destination().withToAddresses(to))
+                .withMessage(new Message()
+                        .withSubject(new Content(subject))
+                        .withBody(new Body().withText(new Content(body))));
+    }
 }
