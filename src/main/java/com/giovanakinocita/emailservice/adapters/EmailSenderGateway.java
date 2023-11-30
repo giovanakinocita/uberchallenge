f6e68b9db3a3c22e7f7f1dc8fb614aa0d282f053
Giovana Kinocita <giovanakinocita@hotmail.com>
2023-11-30 16:53:01 -0300

    Adapters package criated

---
+++ src/main/java/com/giovanakinocita/emailservice/adapters/EmailSenderGateway.java
@@ -0,0 +1,5 @@
+package com.giovanakinocita.emailservice.adapters;
+
+public interface EmailSenderGateway {
+    void sendEmail(String to, String subject, String body);
+}
--- src/main/java/com/giovanakinocita/emailservice/application/EmailSenderService.java
+++ src/main/java/com/giovanakinocita/emailservice/application/EmailSenderService.java
@@ -3,8 +3,14 @@ package com.giovanakinocita.emailservice.application;
 import com.giovanakinocita.emailservice.core.EmailSenderUseCase;
 
 public class EmailSenderService implements EmailSenderUseCase {
+
+    private final EmailSenderGateway emailSenderGateway;
+
+    public EmailSenderService(EmailSenderGateway emailGateway){
+        this.emailSenderGateway = emailGateway;
+    }
     @Override
     public void sendEmail(String to, String subject, String body) {
-        
+
     }
 }
