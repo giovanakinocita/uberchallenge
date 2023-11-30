b1d62acae668ee01cbc660fab68c962137ae2e23
Giovana Kinocita <giovanakinocita@hotmail.com>
2023-11-30 16:46:11 -0300

    Application package criated

---
+++ src/main/java/com/giovanakinocita/emailservice/application/EmailSenderService.java
@@ -0,0 +1,10 @@
+package com.giovanakinocita.emailservice.application;
+
+import com.giovanakinocita.emailservice.core.EmailSenderUseCase;
+
+public class EmailSenderService implements EmailSenderUseCase {
+    @Override
+    public void sendEmail(String to, String subject, String body) {
+        
+    }
+}
